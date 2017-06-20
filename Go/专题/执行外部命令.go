3adb1270724ceeab6423ff0879530e39680e7a9d
gt-sun <sunlorr.gmail.com>
2017-06-20 08:53:21 +0800

    go exec

---
+++ Go/专题/执行外部命令.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+// === 0) 查看命令是否存在===
+func checkExeExists(exe string) {
+	path, err := exec.LookPath(exe)
+	if err != nil {
+		fmt.Printf("didn't find'%s' executable\n", exe)
+		return
+	}
+	fmt.Printf("'%s'executable is'%s'\n", exe, path)
+}
+
+func main() {
+	checkExeExists("ls")
+	checkExeExists("ls2")
+}
+
+// === 1) 最简单的办法===
+func main() {
+	cmd := exec.Command("git", "version")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("combine out:", string(out))
+}
+
+// === 2) stdout stderr 单独捕获===
+func main() {
+	cmd := exec.Command("git", "version")
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Cmd.Run() error %s\n", err)
+	}
+
+	outStr, errStr := stdout.String(), stderr.String()
+	fmt.Printf("out: %s \nerr: %s", outStr, errStr)
+}
+
+// === 3-1) 如果执行过程时间较长，显示执行过长===
+func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			os.Stdout.Write(d)
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
+	}
+	// never reached
+	panic(true)
+	return nil, nil
+}
+
+func main() {
+	cmd := exec.Command("ls", "-lah")
+	var stdout, stderr []byte
+	var errStdout, errStderr error
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	cmd.Start()
+
+	go func() {
+		stdout, errStdout = copyAndCapture(os.Stdout, stdoutIn)
+	}()
+
+	go func() {
+		stderr, errStderr = copyAndCapture(os.Stderr, stderrIn)
+	}()
+
+	err := cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatal("failed to capture stdout or stderr\n")
+	}
+	outStr, errStr := string(stdout), string(stderr)
+	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+}
+
+// === 3.2) 使用io.Copy()
+type CapturingPassThroughWriter struct {
+	buf bytes.Buffer
+	w   io.Writer
+}
+
+// NewCapturingPassThroughWriter creates new CapturingPassThroughWriter
+func NewCapturingPassThroughWriter(w io.Writer) *CapturingPassThroughWriter {
+	return &CapturingPassThroughWriter{
+		w: w,
+	}
+}
+
+// Write writes data to the writer, returns number of bytes written and an error
+func (w *CapturingPassThroughWriter) Write(d []byte) (int, error) {
+	w.buf.Write(d)
+	return w.w.Write(d)
+}
+
+// Bytes returns bytes written to the writer
+func (w *CapturingPassThroughWriter) Bytes() []byte {
+	return w.buf.Bytes()
+}
+
+func main() {
+	var errStdout, errStderr error
+	cmd := exec.Command("ls", "-lah")
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	stdout := NewCapturingPassThroughWriter(os.Stdout)
+	stderr := NewCapturingPassThroughWriter(os.Stderr)
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("cmd.Start() failed with '%s'\n", err)
+	}
+
+	go func() {
+		_, errStdout = io.Copy(stdout, stdoutIn)
+	}()
+
+	go func() {
+		_, errStderr = io.Copy(stderr, stderrIn)
+	}()
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatal("failed to capture stdout or stderr\n")
+	}
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+}
+
+// 3.3) 使用io.MultiWriter()
+func main() {
+	var stdoutBuf, stderrBuf bytes.Buffer
+
+	cmd := exec.Command("ls", "-lah")
+
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+
+	var errStdout, errStderr error
+	stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+	stderr := io.MultiWriter(os.Stderr, &stderrBuf)
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("cmd.Start() failed with '%s'\n", err)
+	}
+
+	go func() {
+		_, errStdout = io.Copy(stdout, stdoutIn)
+	}()
+
+	go func() {
+		_, errStderr = io.Copy(stderr, stderrIn)
+	}()
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	if errStdout != nil || errStderr != nil {
+		log.Fatal("failed to capture stdout or stderr\n")
+	}
+	outStr, errStr := string(stdoutBuf.Bytes()), string(stderrBuf.Bytes())
+	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+}
+
+// === 4) 带环境变量===
+const (
+	envName = "MY_TEST_ENV_VARIABLE"
+)
+
+func runEnvironTest(envValue string) {
+	cmd := exec.Command("go", "run", "05-print-env-helper.go")
+	if envValue != "" {
+		newEnv := append(os.Environ(), fmt.Sprintf("%s=%s", envName, envValue))
+		cmd.Env = newEnv
+	}
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	fmt.Printf("%s", out)
+}
+
+func main() {
+	runEnvironTest("")
+	runEnvironTest("test value")
+}
